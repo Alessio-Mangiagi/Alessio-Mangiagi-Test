d66c3e2dd4bc88c659d4231de9ee3fa101ff881b
Alessio-Mangiagi <160925883+Alessio-Mangiagi@users.noreply.github.com>
2024-05-07 13:01:19 +0200

    esercizi 6/5 python
    esercizi 6/5 python

---
+++ esercizio-6_5.py
@@ -0,0 +1,76 @@
+'''Individuiamo tre task di qualsiasi tipo
+1) Fare la spesa:
+    Identificare i prodotti mancanti;
+     Creare una lista;
+     Andare al supermercato;
+     Prelevare i prodotti presenti sulla lista;
+     Identificare prodotti non presenti in lista ma utili;
+     Pagare alla cassa;
+     Tornare a casa;
+     Conservare i prodotti nelle giuste posizioni.  
+2) Organizzare una escursione con gli amici:
+     Ubriacarsi con gli amici la sera;
+     Farsi venire idee assurde;
+     Andare a letto;
+     Proporre una escursione agli ubriaconi;
+     Verificare i percorsi panoramici;
+     proporre i più belli e interessanti;
+     verificare se i musei o i luoghi di ristoro sono aperti nei giorni selezionati;
+     nel giorno selezionato raggiungere la location;
+     svolgere più attività possibili tra quelle selezionate;
+     tornare stanchi , distrutti e ubriachi a casa.
+3) Cambiare l'olio ad un veicolo a combustione interna:
+     verificare da libbretto il tipo di olio e filtro indicati dal produttore;
+     aquistare in un negorio autorizzaro i maeriali e le attrezzature;   
+     Fermare il veicolo in una struttura rialzata;
+     aspettare che il motore arrivi a temperatura ambiente;
+     porre sotto ul veicolo una vaschetta per raccogliere l'olio esausto;
+     aprire la valvola presente nella coppa ell'olio per far defluire l'olio esausto nella vaschetta;
+     svitare facendo attenzione il vecchio filtro dell'olio;
+     pulire la sede del filtro;
+     utilizzare qualche goccia del nuovo olio per lubrificare la filettatura della sede del filtro;
+     inserire il filtro;
+     chiudere la valvola presente nella coppa dell'olio;
+     inserire il nuovo olio lubrificante nel veicolo;
+     aspettare qualche minuto;
+     controllare il livello dell'olio ;
+     accendere il veicolo e se non si sentono rumori o odori strani aspettare che il motore arrivi a temperatura di esercio;
+     portare l'olio esausto ad un centro per il recupero e il trattamento dei liquidi pericolosi.
+     '''
+print("-"*100)
+è=25
+print(è)
+print("-"*100)
+é=3
+a=è+é
+print(a)
+print("-"*100)
+q="Epicode"
+print(q)
+print("-"*100)
+x=10
+#x=(x+2)*(3)
+#print(x)
+x+=2
+x*=3
+print(x)
+print("-"*100)
+str1 = "Windows" 
+str2 = "Excel" 
+str3 = "Powerpoint" 
+str4 = "Word"
+print(len(str1))
+print(len(str2))
+print(len(str3))
+print(len(str4))
+print("-"*100)
+anno=(((60*60)*24)*365)
+print(anno)
+print("-"*100)
+my_string = "I am studying Python"
+print(my_string.upper())
+print(my_string.lower())
+my_string=my_string.replace("Python","a lot")
+print(my_string)
+print(my_string.strip())
+#non essendoci spazi vuoti ne all'inizio ne alla fine la stringa si presenta nel medesimo modo.
